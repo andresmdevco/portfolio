cbbddc67cd41d15ae0329afd62e5dd52d2a24062
Andrés Muñoz <andres2078@gmail.com>
2026-08-15 13:57:55 -0500

    feat: add home section and responsive web decorations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
       <div className={styles.container}>
         <Header />
+        <Home />
       </div>
     </>
   );
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,28 @@
+import styles from './Home.module.css';
+
+export default function Home() {
+  return (
+      <section className={styles.principal}>
+        <div className={styles.info}>
+          <p>Tu amigable vecino ingeniero de software</p>
+          <h1 className={styles.glitch}>
+            <span>Andrés</span>
+            <span>Muñoz</span>
+          </h1>
+        </div>
+
+        <div className={styles.links}>
+          <a className={styles.projects} href="">
+            Explorar Proyectos
+          </a>
+          <a className={styles.download} href="">
+            Descargar CV
+          </a>
+        </div>
+
+        <div className={styles.webBottom}>
+          <img src="/img/web2.png" alt="" />
+        </div>
+      </section>
+  );
+}
