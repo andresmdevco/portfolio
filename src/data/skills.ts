bcee8eb5e1ec80a3d03c11b2ae032e1afcb0b746
Andrés Muñoz <andres2078@gmail.com>
2026-08-15 22:51:11 -0500

    feat: ad skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,97 @@
+export const skills = [
+  {
+    name: 'Javascript',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'TypeScript',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Python',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Java',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'React',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'HTML5',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'CSS3',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Tailwind CSS',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Bootstrap',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Node.js',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Express.js',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Spring Boot',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'FastAPI',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'PostgreSQL',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'MongoDB',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Git',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'GitHub',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'GitLab',
+    logo: '',
+    category: ''
+  },
+  {
+    name: 'Vscode',
+    logo: '',
+    category: ''
+  },
+]
