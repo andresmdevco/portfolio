c1c1568ec958c1d779b2c33ddad7e4aa5612f03d
Andrés Muñoz <andres2078@gmail.com>
2026-08-28 17:59:35 -0500

    feat: add responsive navigation menu with Heroicons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,37 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid';
 
 export default function Header() {
+  const [nav, setNav] = useState(false);
+
   return (
-    <nav className={styles.nav}>
-      <div className={styles.bar}>
-        <h1 className={styles.title}>
-          <span>&lt;Porta</span>folio/&gt;
-        </h1>
-        <div className={styles.links}>
-          <a href="">Inicio</a>
-          <a href="">Sobre Mí</a>
-          <a href="">Habilidades</a>
-          <a href="">Proyectos</a>
-          <a href="">Certificados</a>
-          <a href="">Contacto</a>
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.bar}>
+          <h1 className={styles.title}>
+            <span>&lt;Porta</span>folio/&gt;
+          </h1>
+
+          <div className={styles.menuButton}>
+            {nav ? (
+              <XMarkIcon onClick={() => setNav(false)} />
+            ) : (
+              <Bars3Icon onClick={() => setNav(true)} />
+            )}
+          </div>
+
+          <div className={`${styles.links} ${nav ? styles.linksOpen : ''}`}>
+            <a href="">Inicio</a>
+            <a href="">Sobre Mí</a>
+            <a href="">Habilidades</a>
+            <a href="">Proyectos</a>
+            <a href="">Certificados</a>
+            <a href="">Contacto</a>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+    </>
   );
 }
