979251c01e59ca64587f6e01f949f2ab77d3879b
Andrés Muñoz <andres2078@gmail.com>
2026-08-29 16:47:15 -0500

    feat: add initial About section structure

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,44 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <p>
+          Sobre Mí
+        </p>
+
+        <h2>
+          Andrés Muñoz
+        </h2>
+
+        <div>
+          <p>
+            ¡Hola! Soy Andrés, soy Ingeniero Electrónico y Especialista en Ingeniería de Software, enfocado en el desarrollo frontend y la construcción de aplicaciones web sólidas y bien pensadas, con experiencia en React, TypeScript y JavaScript.
+          </p>
+          <p>
+            Desde el desarrollo full stack de aplicaciones web hasta la aplicación de buenas prácticas de ingeniería de software, disfruto llevar problemas complejos a soluciones simples y bien construidas.
+          </p>
+        </div>
+
+        <div>
+          <h3>
+            Stack Principal
+          </h3>
+          <ul>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>JavaSript</li>
+            <li>TailwindCSS</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>PostgreSQL</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <img src="/img/foto.png" alt="Foto de Andrés Muñoz" />
+      </div>
+    </div>
   )
 }
