1069f91cf80720184ad7bc013f766de5c8128696
Andrés Muñoz <andres2078@gmail.com>
2026-08-30 20:05:52 -0500

    style: add styles for About section

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
           <Home />
         </section>
       </main>
-      <section className={styles.section}>
+      <section className={styles.sectionAbout}>
         <About />
       </section>
     </>
